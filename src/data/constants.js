bd2663356804c86db29d3c9aa2cf7b558385a979
ProjectExcelr <projectexcelr123@gmail.com>
2025-02-06 10:15:14 +0530

    Seshu Portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,204 @@
+export const Bio = {
+  name: "Pavan Seshu Akula",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Seshu916",
+  resume:
+  "https://drive.google.com/file/d/1sJbA59eO7QzTXpINPp66_BQ9TllR-nD1/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/pavan-seshu-akula-194479270/",
+  twitter: "https://x.com/PavanSeshu1",
+  insta: "https://www.instagram.com/seshu_______/",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://w7.pngwing.com/pngs/28/601/png-transparent-sql-logo-illustration-microsoft-azure-sql-database-microsoft-sql-server-database-blue-text-logo-thumbnail.png",
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Spring",
+        image:
+          "https://img.icons8.com/?size=512&id=90519&format=png",
+      },
+      {
+        name: "SpringBoot",
+        image:
+          "https://dz2cdn1.dzone.com/storage/temp/12434118-spring-boot-logo.png",
+      },
+      {
+        name: "Hibernate",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/hibernate.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/postman.svg",
+      },
+      {
+        name: "Spring Tool Suite(STS)",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRtQyXi1necbFlJOetK3_3MHaLeHDGZ-C3pw&s",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.aivariant.com/wp-content/uploads/2021/01/ai_logo5_2-1.png",
+    role: "Full Stack Developer Developer Intern",
+    company: "AI VARIANT",
+    date: "JAN 2024 - JUN 2024",
+    desc: "Worked on react front-end and development and Integration.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://careergalaxy.allegiance-educare.in/storage/uploads/colleges/thumb/250_250_1479541776RIT.jpg",
+    school: "Raghu Institute Of Technology , Vishakapatnam",
+    date: "nov 2020 - apr 2023",
+    grade: "7.96 CGPA",
+    desc: "I had completed my Bachelor's degree in Electrical and Electronics Engineering at Raghu Institute of Technology, Vishakapatnam. I had secured an overall CGPA of 7.96 I have taken courses in Core Java,HTML5,CSS3",
+    degree: "Bachelor of Technology - BTech, Electrical and Electronics Engineering",
+  },
+  {
+    id: 1,
+    img: "https://play-lh.googleusercontent.com/ltjak6wyekUfzFGPi7hs5AHyApkJbHXylN-Woc7zBZmq9pfZcRzPGKtic_HMIZZKZdE",
+    school: "Smt.B.Seetha Polytechnic , Bhimavaram",
+    date: "June 2017 - Apr 2020",
+    grade: "73.8%",
+    desc: "I completed my Diploma at Smt.B.Seetha Polytechnic, Bhimavaram, where I studied Electrical and Electronics Engineering.",
+    degree: "Diploma, Electrical and Electronics Engineering",
+  },
+  {
+    id: 2,
+    img: "https://img.freepik.com/free-vector/gradient-high-school-logo-design_23-2149626932.jpg?size=338&ext=jpg&ga=GA1.1.2008272138.1726185600&semt=ais_hybrid",
+    school: "S.M.B.T.R.Z.P.High School, Deva",
+    date: "June 2016 - Apr 2017",
+    grade: "8.3 CGPA",
+    desc: "I completed my class 10 education at S.M.B.T.R.Z.P.High School, Deva, where I SSC.",
+    degree: "SSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Green Bucket Store",
+    date: "OCT 2024",
+    description:
+    "An Application for Optimize delivery processes, reduce delivery times, minimize environmental impact, and enhance customer satisfaction in the online grocery industry",
+    image:
+      "https://budgro.wordpress.com/wp-content/uploads/2015/06/q.jpg",
+    tags: ["React JS", "Java","Spring Boot","Hibernate","MySql"],
+    category: "Full Stack Development",
+    github: "hhttps://github.com/Seshu916/Green_Bucket_Store",
+    webapp: "https://github.com/Seshu916/Green_Bucket_Store",
+  },
+  {
+    id: 3,
+    title: "Bus Ticket Reservation System",
+    date: "Jan 2023",
+    description:
+    "A Console based Bus Ticket Booking System Where users can interact with console and Book Tickets for the available routes it can also generate a payslip based on the booking cost.",
+    image:
+      "https://user-images.githubusercontent.com/110565344/221412365-74a20f45-5d8d-4b5d-8549-1f9c6c14405a.png",
+    tags: ["Core Java", "Collections","Eclipse"],
+    category: "Core Java",
+    github: "https://github.com/Seshu916/Travel-Desk",
+    webapp: "https://github.com/Seshu916/Travel-Desk",
+  },
+  
+];
+
+export const TimeLineData = [
+  { year: 2022, text: "Started my journey" },
+  { year: 2023, text: "Done a course on Core Java" },
+  { year: 2023, text: "Learned HTML,CSS,Java Script" },
+  { year: 2023, text: "Done a course on Full Stack Java Development" },
+  { year: 2024, text: "Worked on Real time Project" },
+];
